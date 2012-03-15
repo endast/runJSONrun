93f04bef2f483bb8787eb4592e95bef6dc31ce43
Magnus Wahlberg <endast@gmail.com>
2012-03-15 20:29:25 +0100

    Refactored the output code

--- runJSONrun.py
+++ runJSONrun.py
@@ -9,6 +9,7 @@ Copyright (c) 2012 __RedSwampResarch__. All rights reserved.
 
 from getopt import getopt, GetoptError
 from sys import argv, exit, platform
+import sys
 import os
 import csv
 import json
@@ -25,8 +26,28 @@ def cleanUpHeaders(csvData):
 	
 	return csvData
 
+def writeJSON(JSONdata, outputFile):
+	
+	# No outputfile specified, dump to stdout
+	if outputFile == None:
+		print JSONdata
+	else:
+		print "Writing JSON data to", outputFile
+		
+		jsonFile = open(outputFile,"w")
+		jsonFile.writelines(JSONdata)
+		jsonFile.close()
+
 def usage():
-	print "Usage: remotestick-server [OPTION] ..."
+	print "\nUsage: runJSONrun.py file.csv ..."
+	print "Convert runmeter (http://www.abvio.com/runmeter/) csv files to JSON."
+	print ""
+	print "If you start runJSONrun and only specify a csv file, it will dump the JSON data to standard out."
+	print ""
+	print "-?, \t\tPrint this help and exit..."
+	print "-i, --input\tThe runmeter CSV file to read (needed only if you wan't to write to a file)"
+	print "-o, --output\tThe file you wan't the JSON data to be written to"
+	print "-V, \t\tPrint the version and exit\n"
 
 def version():
 	print "runJSONrun v" + VERSION
@@ -34,7 +55,7 @@ def version():
 def main():
 
 	try:
-		opts, args = getopt(argv[1:], "?hi:o:V", ["?", "input=", "output="])
+		opts, args = getopt(argv[1:], "?hi:o:V", ["?", "input=", "output=", "--version"])
 	except GetoptError, err:
 		print str(err)
 		usage()
@@ -45,44 +66,40 @@ def main():
 	# If we only have one argument, we assume its the csv file
 	if len(argv) == 2:
 		inputFile = argv[1]
+	elif len(argv) == 1:
+		usage()
+		exit()
 		
 	for o, a in opts:
 		if o in ("-i", "--input"):
 			inputFile = a
 		elif o in ("-o", "--output"):
 			outputFile = a
+		elif o in ("-V", "--version"):
+			version()
+			exit()
 		elif o == "-?":
 			usage()
 			exit()
 		else:
 			assert False, "unhandled option " + o
-
-	RunMeterCSVFile = inputFile
-
+			
 	if inputFile != None:
 	
-		RawcsvData = getCSVData(RunMeterCSVFile)
+		RawcsvData = getCSVData(inputFile)
 		csvData = cleanUpHeaders(RawcsvData)
 		
-		points = []
+		# Add all data to an array
+		dataPoints = []
 		for row in csvData:
-			points.append(row)
+			dataPoints.append(row)
 		
-		jsonData = json.dumps(points)
+		jsonData = json.dumps(dataPoints)
 		
-		# No outputfile specified, dump to stdout
-		if outputFile == None:
-			print jsonData
-		else:
-			print "Writing JSON data to", outputFile
+		writeJSON(jsonData, outputFile)
 		
-			jsonFile = open(outputFile,"w")
-			jsonFile.writelines(jsonData)
-			jsonFile.close()
-			
-			
 	else:
-		print "Could not read input file"
+		print "Could not read input file... exiting."
 		exit(2)
 			
 if __name__ == '__main__':
